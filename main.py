e5e89682779867b15c663674b0d306530961dc71
jce2025 <jcesperante@cmc.pshs.edu.ph>
2026-03-17 08:11:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 name = input("What is your name? ")
-print("Hello, " + name + "!")
+age = input("How old are you?")
+address = input("Where do you live?")
+print(name + "is" + age + " years old and lives in " + address+".")
